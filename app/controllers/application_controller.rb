4bfe50752690bf8e1dd7eedc36211c003e990a8f
J Lee <jay.j.lee.1@gmail.com>
2022-02-28 19:23:50 -0500

    completed but react app broken

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   include ActionController::Cookies
 
+  private
+
+  def record_not_found
+    render json: { error: "Article not found" }, status: :not_found
+  end
+  
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
@@ -11,10 +10,6 @@ class ArticlesController < ApplicationController
     render json: article
   end
 
-  private
-
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
-  end
+  
 
 end
